88576606eefdc0aeb8772ef08126ff2d98c36d4c
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-12-10 18:01:39 +0100

    Initial commit

---
+++ generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from random import shuffle
+
+teams = []
+
+with open('teams.txt', 'r') as f, open('random_teams.txt', 'w') as r:
+     teams = [i.strip() for i in f.readlines()]
+     shuffle(teams)
+     shuffle(teams)
+     shuffle(teams)
+     shuffle(teams)
+     for team in teams:
+         r.write("{}\n".format(team))
