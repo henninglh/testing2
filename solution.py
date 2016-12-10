23f43d6d5b9731f363ba073ac0e890c476732f6d
Henning Lund-Hanssen <henning.lund.hanssen@gmail.com>
2016-12-10 23:24:45 +0100

    Finalized

--- solution.py
+++ solution.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 from toposort import toposort
 from collections import defaultdict
-
 network = defaultdict(set)
-
 with open("input.txt", "r") as f:
     lines = [i for i in f.readlines()]
     for line in lines:
         source, target = [i.strip() for i in line.split(">")]
         network[source].add(target)
-result = list(toposort(network))
-#print result
-for res in result:
-    for r in res:
-        print "{},".format(r),
-    print "---"
+print list(toposort(network))
